a38aa96434336bd07bd1939048e34379457c4afd
Kedarnath Peraka <71390186+kedarnathdev@users.noreply.github.com>
2025-02-13 23:04:36 +0530

    Update emoji.ts
    Adding capital Y and capital N

--- app/emoji.ts
+++ app/emoji.ts
@@ -23,4 +23,4 @@ export const EMOJI_LIST = [
     "🥹",
 ]
 
-export const ALPHABET_LIST = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+export const ALPHABET_LIST = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'Y', 'N']
